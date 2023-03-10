d7d2af61940cdba091de91b15e4b2208529a96a1
tam.nguyen <tamthanh770@gmail.com>
2023-03-11 00:17:57 +0700

    Init commit

---
+++ src/containers/skillProgress/skillProgress.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Progress.scss";
+import {illustration, techStack} from "../../portfolio";
+import {Fade} from "react-reveal";
+import Build from "../../assets/lottie/build";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import { Tooltip } from 'react-tooltip';
+
+export default function StackProgress() {
+  if (techStack.viewSkillBars) {
+    return (
+      <Fade bottom duration={1000} distance="20px">
+        <div className="skills-container">
+          <div className="skills-bar">
+            <h1 className="skills-heading">Proficiency</h1>
+            {techStack.experience.map((exp, i) => {
+              const progressStyle = {
+                width: exp.progressPercentage
+              };
+              return (
+                <div key={i} className="skill">
+                  <p>{exp.Stack}</p>
+                  <div className="meter" data-tooltip-id="my-tooltip" data-tooltip-content={exp.title}>
+                    <span style={progressStyle}></span>
+                  </div>
+                  <Tooltip id="my-tooltip" />
+                </div>
+              );
+            })}
+          </div>
+          <div className="skills-image">
+            {illustration.animated ? (
+              <DisplayLottie animationData={Build} />
+            ) : (
+              <img
+                alt="Skills"
+                src={require("../../assets/images/skill.svg")}
+              />
+            )}
+          </div>
+        </div>
+      </Fade>
+    );
+  }
+  return null;
+}
